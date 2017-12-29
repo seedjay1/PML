7051e07d3924ad4979344028ba2570c0b09bee2a
seedjay1 <see_d_jay@yahoo.com>
2017-12-29 09:56:39 -0800

    just scribble following the lectures

---
+++ week2lecture.R
@@ -0,0 +1,77 @@
+# following along with the Week 2 lecture
+
+# set up the data
+library(caret)
+library(kernlab)
+data(spam)
+inTrain <- createDataPartition(y = spam$type
+                               , p = .75
+                               , list = FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+
+
+set.seed(42)
+folds <- createFolds(y = spam$type
+                     , k = 10
+                     , list = TRUE
+                     , returnTrain = TRUE)
+sapply(folds, length)
+
+# only difference is returntrain
+set.seed(42)
+folds <- createFolds(y = spam$type
+                     , k = 10
+                     , list = TRUE
+                     , returnTrain = FALSE)
+sapply(folds, length)
+
+
+# resample
+set.seed(42)
+folds <- createResample(y = spam$type
+                        , times = 10
+                        , list = TRUE)
+sapply(folds, length)
+
+############
+
+# plotting predictors (wage data)
+
+library(ISLR)
+data(Wage)
+
+inTrain <- createDataPartition(y = Wage$wage
+                               , p = .7
+                               , list = FALSE)
+training <- Wage[inTrain,]
+testing <- Wage[-inTrain,]
+
+# feature plot
+fp <- featurePlot(x = training[,c("age", "education", "jobclass")]
+            , y = training[, "wage"]
+            , plot = "pairs")
+print(fp)
+
+# qplot
+qp <- qplot(age, wage, data=training)
+print(qp)
+
+qp2 <- qplot(age, wage, color=jobclass, data=training)
+print(qp2)
+
+qp3 <- qplot(age, wage, color=education, data=training)
+qp3 <- qp3 + geom_smooth(method="lm", formula=y~x)
+print(qp3)
+
+
+# make wage bins for separate analysis
+library(Hmisc)
+cutWage <- cut2(training$wage, g=3)
+print(table(cutWage))
+
+bp1 <- qplot(cutWage, age, data=training, fill=cutWage, geom=c("boxplot"))
+print(bp1)
+
+# NOW TRY THIS WITH A VIOLIN PLOT IN ORDER TO SEE THE POINTS
+
